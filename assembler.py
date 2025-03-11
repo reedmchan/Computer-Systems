4196c6a2bc1fab5799a01cf097d76d07ae2f2407
reedmchan <134111076+reedmchan@users.noreply.github.com>
2025-03-11 00:35:52 -0700

    Update assembler.py

--- assembler.py
+++ assembler.py
@@ -11,7 +11,9 @@ class Y86Assembler:
         }
         self.registers = {
             '%rax': '0', '%rcx': '1', '%rdx': '2', '%rbx': '3',
-            '%rsp': '4', '%rbp': '5', '%rsi': '6', '%rdi': '7'
+            '%rsp': '4', '%rbp': '5', '%rsi': '6', '%rdi': '7',
+            '%r8': '8', '%r9': '9', '%r10': 'A', '%r11': 'B',
+            '%r12': 'C', '%r13': 'D', '%r14': 'E'
         }
         self.labels = {}
         self.memory = {}
@@ -22,19 +24,26 @@ class Y86Assembler:
             self.address = int(line.split()[1], 16)
         elif line.startswith('.align'):
             align = int(line.split()[1])
-            self.address = (self.address + align - 1) // align * align
-        elif line.startswith('.long') or line.startswith('.quad'):
+            padding = (align - (self.address % align)) % align
+            for _ in range(padding):
+                self.memory[self.address] = '00'
+                self.address += 1
+        elif line.startswith('.quad'):
             value = int(line.split()[1], 0)
-            self.memory[self.address] = f'{value:016x}'
-            self.address += 8
+            hex_value = f'{value:016x}'
+            for i in range(0, 16, 2):
+                self.memory[self.address] = hex_value[i:i+2]
+                self.address += 1
         elif line.startswith('.byte'):
             value = int(line.split()[1], 0)
             self.memory[self.address] = f'{value:02x}'
             self.address += 1
         elif line.startswith('.word'):
             value = int(line.split()[1], 0)
-            self.memory[self.address] = f'{value:04x}'
-            self.address += 2
+            hex_value = f'{value:04x}'
+            for i in range(0, 4, 2):
+                self.memory[self.address] = hex_value[i:i+2]
+                self.address += 1
 
     def assemble(self, lines):
         for line in lines:
@@ -46,8 +55,8 @@ class Y86Assembler:
             if line.startswith('.'):
                 self.parse_directive(line)
             elif line:
-                self.address += 1  
-        
+                self.address += 10
+
         self.address = 0
         output = []
         for line in lines:
@@ -55,38 +64,46 @@ class Y86Assembler:
             if ':' in line:
                 _, line = line.split(':', 1)
             line = line.strip()
+
             if line.startswith('.'):
                 self.parse_directive(line)
             elif line:
-                parts = line.split()
+                parts = line.split(None, 1)
                 opcode = self.instructions.get(parts[0])
                 if opcode:
                     operands = ''
                     if len(parts) > 1:
-                        operands_list = parts[1].replace(',', '').split()
+                        operands_list = re.split('[,\s]+', parts[1])
                         for op in operands_list:
                             if op in self.registers:
                                 operands += self.registers[op]
                             elif op in self.labels:
-                                operands += f'{self.labels[op]:04x}'
-                            else:
-                                operands += f'{int(op.strip("$"), 0):02x}'
-                    output.append(f'{self.address:04x}: {opcode} {operands}')
-                    self.address += 1
-        
+                                operands += f'{self.labels[op]:016x}'
+                            elif op.startswith('$'):
+                                operands += f'{int(op[1:], 0):016x}'
+                    machine_code = f'{opcode}{operands}'
+                    output.append(f'{self.address:04x}: {machine_code}')
+                    self.address += len(machine_code) // 2
+
+        if self.address < 0x7c:
+            output.append(f'{0x7c:04x}: (STACK START)')
+
         return '\n'.join(output)
 
 # Ex
 code = [
     '.pos 0x100',
-    'start: irmovq $10, %rax',
+    'init: pushq %rbp',
+    'call main',
+    'halt',
+    'main: irmovq $10, %rax',
     'addq %rbx, %rax',
-    'jmp start',
+    'jmp init',
     '.quad 0x12345678',
     '.byte 0xff',
     '.word 0x1234',
     'pushq %rbp',
-    'call start'
+    'call main'
 ]
 
 assembler = Y86Assembler()
